ae154ee26d64306569dff9e864c0f7d8e64baaca
tom <ychen306@mit.edu>
2021-02-26 13:23:26 -0500

    got abs working

--- src/main.rs
+++ src/main.rs
@@ -61,18 +61,77 @@ fn trunc_binary(op: &str) -> Vec<egg::Rewrite<LLVM, ()>> {
     build_rewrite2(&name, &lhs, &rhs)
 }
 
+fn power_of_two_ceil(n: u32) -> u32 {
+    (n as f64).log2().ceil().powi(2) as u32
+}
+
+// FIXME: instead of truncating the the most precise, quantize it to power-of-2
+fn add_precise(old_bw: u32, new_bw: u32) -> Vec<egg::Rewrite<LLVM, ()>> {
+    let small = power_of_two_ceil(old_bw + 1);
+    if small == new_bw && false {
+        Vec::new()
+    } else {
+        let name = format!("sub-precise-{}-{}", old_bw, new_bw);
+        let lhs = format!(
+            "(sub {new} (sext {old} {new} ?x) (sext {old} {new} ?y))",
+            old = old_bw,
+            new = new_bw
+        );
+        let rhs = format!(
+            "(sext {small} {new}
+                           (sub {small} (sext {old} {small} ?x) (sext {old} {small} ?y)))",
+            old = old_bw,
+            small = small,
+            new = new_bw
+        );
+        build_rewrite2(&name, &lhs, &rhs)
+    }
+}
+
+fn cmp_sext(old_bw: u32, new_bw1: u32, new_bw2: u32) -> Vec<egg::Rewrite<LLVM, ()>> {
+    if old_bw > new_bw1 || old_bw > new_bw2 || new_bw1 >= new_bw2 {
+        return Vec::new();
+    }
+
+    let cmps = ["slt", "sle", "sgt", "sge", "eq"];
+    let mut rules = Vec::new();
+
+    for cmp in cmps.iter() {
+        let name = format!("{}-sext-{}-{}-{}", cmp, old_bw, new_bw1, new_bw2);
+        let lhs = if new_bw1 == old_bw {
+            format!("({cmp} {old} ?x ?y)", old = old_bw, cmp = cmp)
+        } else {
+            format!(
+                "({cmp} {new} (sext {old} {new} ?x) (sext {old} {new} ?y))",
+                cmp = cmp,
+                old = old_bw,
+                new = new_bw1
+            )
+        };
+        let rhs = format!(
+            "({cmp} {new} (sext {old} {new} ?x) (sext {old} {new} ?y))",
+            cmp = cmp,
+            old = old_bw,
+            new = new_bw2
+        );
+        rules.extend(build_rewrite2(&name, &lhs, &rhs));
+    }
+
+    rules
+}
+
 fn rules() -> Vec<egg::Rewrite<LLVM, ()>> {
     let mut r = vec![
         rw!("add-assoc"; "(add ?bw ?a ?b)" => "(add ?bw ?b ?a)"),
         rw!("mul-assoc"; "(mul ?bw ?a ?b)" => "(mul ?bw ?b ?a)"),
         rw!("add-comm"; 
-          "(add ?bw (add ?bw ?a ?b) ?c)"
-          =>
-          "(add ?bw ?a (add ?bw ?b ?c))"),
+        "(add ?bw (add ?bw ?a ?b) ?c)"
+        =>
+        "(add ?bw ?a (add ?bw ?b ?c))"),
         rw!("mul-comm"; 
-          "(mul ?bw (mul ?bw ?a ?b) ?c)"
-          =>
-          "(mul ?bw ?a (mul ?bw ?b ?c))"),
+            "(mul ?bw (mul ?bw ?a ?b) ?c)"
+            =>
+            "(mul ?bw ?a (mul ?bw ?b ?c))"),
     ];
     r.extend(
         vec![
@@ -80,10 +139,10 @@ fn rules() -> Vec<egg::Rewrite<LLVM, ()>> {
             trunc_binary("sub"),
             trunc_binary("mul"),
             rw!(
-               "trunc-select";
-               "(trunc ?old ?new (select ?old ?cond ?t ?f))"
-               <=>
-               "(select ?new ?cond (trunc ?old ?new ?t)
+      "trunc-select";
+      "(trunc ?old ?new (select ?old ?cond ?t ?f))"
+      <=>
+      "(select ?new ?cond (trunc ?old ?new ?t)
                                    (trunc ?old ?new ?f))"),
             rw!("slt-sgt"; "(slt ?bw ?a ?b)" <=> "(sgt ?bw ?b ?a)"),
             rw!("sle-sge"; "(sle ?bw ?a ?b)" <=> "(sge ?bw ?b ?a)"),
@@ -101,8 +160,10 @@ fn rules() -> Vec<egg::Rewrite<LLVM, ()>> {
                 let lhs = format!("(sext {i} {j} (const {i} 0))", i = i, j = j);
                 let rhs = format!("(const {j} 0)", j = j);
                 r.extend(build_rewrite2(&name, &lhs, &rhs));
+                r.extend(add_precise(*i, *j));
             }
             for k in bitwidths.iter() {
+                r.extend(cmp_sext(*i, *j, *k));
                 // ((? j k (? i j x)))
                 if k < j && j > i {
                     let lhs = format!("(trunc {j} {k} (sext {i} {j} ?x))", i = i, j = j, k = k);
@@ -209,23 +270,50 @@ fn trunc_sext_add2() {
 }
 
 #[test]
-fn trunc_abs() {
+fn test_sub_precise() {
+    assert!(is_equivalent(
+        "(sub 32 (sext 8 32 x) (sext 8 32 y))",
+        "(sext 16 32 (sub 16 (sext 8 16 x) (sext 8 16 y)))"
+    ));
+}
+
+#[test]
+fn test_cmp_sext() {
+    assert!(is_equivalent(
+        "(slt 16 (sub 16 (sext 8 16 x) (sext 8 16 y)) (const 16 0))",
+        "(slt 32 (sub 32 (sext 8 32 x) (sext 8 32 y)) (const 32 0))"
+    ));
+}
+
+fn abs16() -> String {
     let diff16 = "(sub 16 (sext 8 16 x) (sext 8 16 y))";
+    format!(
+        "(select 16 (slt 16 {diff16} (const 16 0))
+                    {diff16}
+                    (sub 16 (sext 8 16 y) (sext 8 16 x)))",
+        diff16 = diff16)
+}
+
+fn abs32() -> String {
     let diff32 = "(sub 32 (sext 8 32 x) (sext 8 32 y))";
-    let abs16 = format!(
-        "(select 16 foo 
-                     {diff16} 
-                     (sub 16 (sext 8 16 y) (sext 8 16 x)))",
-        diff16 = diff16
-    );
-    let abs16_2 = format!(
-        "(trunc 32 16
-                (select 32 foo 
-                           {diff32}
-                           (sub 32 (sext 8 32 y) (sext 8 32 x))))",
-        diff32 = diff32
-    );
-    assert!(is_equivalent(&abs16, &abs16_2));
+    format!(
+        "(select 32 (slt 32 {diff32} (const 32 0))
+                    {diff32}
+                    (sub 32 (sext 8 32 y) (sext 8 32 x)))",
+        diff32 = diff32)
+}
+
+#[test]
+fn trunc_abs() {
+    let abs16_2 = format!("(trunc 32 16 {})", abs32());
+    assert!(is_equivalent(&abs16(), &abs16_2));
+}
+
+#[test]
+fn add_abs() {
+  let sad16 = format!("(add 16 {x} {x})", x=abs16());
+  let sad32 = format!("(trunc 32 16 (add 32 {x} {x}))", x=abs32());
+  assert!(is_equivalent(&sad16, &sad32));
 }
 
 #[test]
@@ -245,5 +333,8 @@ fn reorder_mul() {
 }
 
 fn main() {
-    println!("Hello, world!");
+  let sad16 = format!("(add 16 {x} {x})", x=abs16());
+  let sad32 = format!("(trunc 32 16 (add 32 {x} {x}))", x=abs32());
+  println!("sad16 = {}", sad16);
+  println!("sad32 = {}", sad32);
 }
